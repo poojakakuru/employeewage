a16f96d09949cea5444165e50a5fc2dfa83c2297
poojakakuru <poojareddykakuru@gmail.com>
2020-10-05 18:41:49 +0530

    Employee wage in switchcase program done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -40,3 +40,21 @@ else
 fi
 salary=$(( $empHrs*$empRatePerHr ));
 
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+empCheck=$(( RANDOM%3 ));
+
+case $empCheck in
+	$isFullTime)
+					empHrs=8
+					;;
+	$isPartTime)
+					empHrs=4
+					;;
+	*)
+					empHrs=0
+					;;
+esac
+salary=$(( $empHrs*$empRatePerHr ));
+
