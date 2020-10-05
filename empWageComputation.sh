f3322d0181939130f55235701310c6318b0b41b9
poojakakuru <poojareddykakuru@gmail.com>
2020-10-05 17:48:27 +0530

    Daily Employee wage program done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,3 +11,15 @@ then
 else
     echo "Employee is absent";
 fi
+
+isPresent=1;
+randomCheck=$(( RANDOM%2 ));
+
+if [ $isPresent -eq $randomCheck ];
+then
+   empRatePerHr=20;
+	empHrs=8;
+	salary=$(($empHrs*$empRatePerHr));
+else
+	salary=0;
+fi
