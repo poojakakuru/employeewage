122d836ccdfefa03cbfc2fcb89d2188fb1d05b63
poojakakuru <poojareddykakuru@gmail.com>
2020-10-05 17:27:26 +0530

    Attendance problem done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to the Employee wage program"
+
+isPresent=1;
+randomCheck=$(( RANDOM%2 ));
+
+if [ $isPresent -eq $randomCheck ];
+then
+    echo "Employee is Present";
+else
+    echo "Employee is absent";
+fi
