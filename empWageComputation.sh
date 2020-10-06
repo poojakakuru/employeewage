19820e2c0942796a6ebf82b798c9545e934dbb70
poojakakuru <poojareddykakuru@gmail.com>
2020-10-06 14:07:16 +0530

    employee wage array program done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -141,3 +141,43 @@ do
 	totalEmpHrs=$(($totalEmpHrs+$empHrs))
 	totalSalary=$(($totalSalary+($totalEmpHrs*$empRatePerHr)))
 done
+
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=10
+empRatePerHr=20
+numWorkingDays=20
+
+totalEnpHrs=0
+totalWorkingDays=0
+
+function getWorkingHours() {
+	case $1 in
+		$isPartTime)
+			empHrs=4
+			;;
+		$isFullTime)
+			empHrs=8
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	echo $empHrs
+}
+function calDailyWages() {
+	totalWorkHrs=$1;
+	wages=$(( $totalWorkHrs*$empRatePerHr ))
+	echo $wages;
+}
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+	totalWorkingDays=$(($totalWorkingDays+1))
+	empHrs="$(getWorkingHrs$((RANDOM%3)) )"
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	empDailyWages[$totalWorkingDays]="$(calDailyWages$empHrs)"
+done
+totalSalary="$(calDailyWages $totalEmpHrs)"
+echo "daily wages :"
+    ${empDailyWages[@]}
+
