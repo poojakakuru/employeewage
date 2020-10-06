88d793e7da973228fa025a5e697087440bec696e
poojakakuru <poojareddykakuru@gmail.com>
2020-10-06 12:47:40 +0530

    Totalworking hours of employee wage program done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -81,3 +81,32 @@ do
 		salary=$(($empHrs*$empRatePerHr));
 		totalSalary=$(($totalSalary+$salary));
 done
+
+
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=10
+empRatePerHr=20
+numWorkingDays=20
+
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+	totalWorkingDays=$(($totalWorkingDays+1))
+	empCheck=$(( RANDOM%3 ))
+	case $empCheck in
+		$isPartTime)
+			empHrs=4
+			;;
+		$isFullTime)
+			empHrs=8
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	totalSalary=$(($totalSalary+($totalEmpHrs*$empRatePerHr)))
+done
