680f98c2f2d052830d0fa282703271aeaedd5b43
poojakakuru <poojareddykakuru@gmail.com>
2020-10-06 13:10:11 +0530

    Employeewage function program done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -110,3 +110,34 @@ do
 	totalEmpHrs=$(($totalEmpHrs+$empHrs))
 	totalSalary=$(($totalSalary+($totalEmpHrs*$empRatePerHr)))
 done
+
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=10
+empRatePerHr=20
+numWorkingDays=20
+
+totalEmpHrs=0
+totalWorkingDays=0
+
+function getWorkingHours(){
+	case $1 in
+		$isPartTime)
+			empHrs=4
+			;;
+		$isFullTime)
+			empHrs=8
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	echo $empHrs
+}
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+	totalWorkingDays=$(($totalWorkingDays+1))
+	empHrs="$(getWorkingHours $(( RANDOM%3 )) )"
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	totalSalary=$(($totalSalary+($totalEmpHrs*$empRatePerHr)))
+done
